ed772bf3581f3ad062a9f90bb0bd1cd3bf5d7482
Andyyyyyychen <2524103207@qq.com>
2026-06-08 21:43:40 -0400

    Add RSL-RL config compatibility shim

--- robot_lab-main/source/robot_lab/robot_lab/tasks/__init__.py
+++ robot_lab-main/source/robot_lab/robot_lab/tasks/__init__.py
@@ -10,12 +10,41 @@
 
 import os
 import toml
+import inspect
 
 from isaaclab_tasks.utils import import_packages
 
 ##
 # Register Gym environments.
 ##
+
+
+def _patch_rsl_rl_actor_critic_cfg() -> None:
+    """Keep robot_lab configs importable across nearby Isaac Lab / RSL-RL versions."""
+    try:
+        from isaaclab_rl.rsl_rl import RslRlPpoActorCriticCfg
+    except Exception:
+        return
+
+    init_signature = inspect.signature(RslRlPpoActorCriticCfg.__init__)
+    unsupported_normalization_args = {
+        "actor_obs_normalization",
+        "critic_obs_normalization",
+    } - set(init_signature.parameters)
+    if not unsupported_normalization_args:
+        return
+
+    original_init = RslRlPpoActorCriticCfg.__init__
+
+    def compatible_init(self, *args, **kwargs):
+        for name in unsupported_normalization_args:
+            kwargs.pop(name, None)
+        original_init(self, *args, **kwargs)
+
+    RslRlPpoActorCriticCfg.__init__ = compatible_init
+
+
+_patch_rsl_rl_actor_critic_cfg()
 
 
 # The blacklist is used to prevent importing configs from sub-packages
